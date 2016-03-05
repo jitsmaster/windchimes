bcec165391e457c5e9e23b212dfdaaa6c22fd2c9
Tero Parviainen <tero@teropa.info>
2016-03-05 14:45:39 +0200

    Add animated transitions

--- src/bell.component.ts
+++ src/bell.component.ts
@@ -1,20 +1,30 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, OnInit} from 'angular2/core';
 import {Location} from './location';
 
 @Component({
   selector: 'bell',
   template: `
     <div class="bell"
-         [style.transform]="getTransform()">
+         [style.transform]="getTransform()"
+         [style.opacity]="getOpacity()">
     </div>
   `,
   styles: [require('./bell.component.css').toString()]
 })
-export class Bell {
+export class Bell implements OnInit {
   @Input() location:Location;
+  fade:boolean;
 
+  ngOnInit() {
+    setTimeout(() => this.fade = true, 200);
+  }
   getTransform() {
-    return `translate(${this.location.x}px, ${this.location.y}px)`;
+    const translate = `translate3d(${this.location.x}px, ${this.location.y}px, 0)`;
+    const scale = this.fade ? 'scale3d(300, 300, 300)' : 'scale3d(1, 1, 1)';
+    return `${translate} ${scale}`;
+  }
+  getOpacity() {
+    return this.fade ? 0 : 0.7;
   }
 
 }
