3e6a0a389505d03b307d02eb3f40ac90f3f9e762
Tero Parviainen <tero@teropa.info>
2016-03-13 18:57:00 +0200

    Prod build support

---
+++ webpack.prod.config.js
@@ -0,0 +1,33 @@
+var autoprefixer = require('autoprefixer');
+var path = require('path');
+
+module.exports = {
+  entry: ['./src/polyfills.ts', './src/main.ts'],
+  resolve: {
+    extensions: ['', '.ts', '.js'],
+    root: [
+      path.resolve('../angular/dist/js/cjs'),
+      path.join(__dirname, "node_modules")
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'babel?presets[]=es2015!ts'
+      },
+      {test: /\.css$/, loader: 'css-loader!postcss-loader'}
+    ]
+  },
+  postcss: function() {
+    return [autoprefixer];
+  },
+  ts: {
+    configFileName: 'tsconfig.prod.json'
+  },
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'cheap-source-map'
+};
