872e5a64b4715076d6f26605c99305bd51a33dc3
Tero Parviainen <tero@teropa.info>
2016-04-09 20:44:33 +0300

    Include web animations polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
-
+import 'web-animations-js';
 require('stereo-panner-node').polyfill();
 
 require('rxjs/Rx');
