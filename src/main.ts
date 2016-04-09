b72e63ca54d1fca9e77a197f62094d7888fdd7e6
Tero Parviainen <tero@teropa.info>
2016-04-09 21:40:49 +0300

    don't pull in http providers

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ChangeDetectorGenConfig} from 'angular2/src/core/change_detection/interfaces';
-import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent, [
   provide(ChangeDetectorGenConfig, { useValue: new ChangeDetectorGenConfig(true, false, false)}),
-  HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
   provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
