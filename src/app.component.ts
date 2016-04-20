be9126d3f08895c51de743b10f991794202ee690
Tero Parviainen <tero@teropa.info>
2016-04-20 06:38:19 +0300

    loading indicator wip

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, Inject, provide} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {style, animate} from 'angular2/animate';
+import {LoadingIndicator} from './loading-indicator.component';
 import {Windchimes} from './windchimes.component';
 import {WindchimesRemote} from './windchimes-remote.component';
 import {WindchimesInteractive} from './windchimes-interactive.component';
@@ -12,11 +14,19 @@ import {Samples} from './samples.service';
   selector: 'wind-chimes-app',
   template: `
     <div (window:resize)="onWindowResize()">
-      <router-outlet></router-outlet>
+      <router-outlet *ngIf="!isLoading()"></router-outlet>
+      <loading-indicator *ngIf="isLoading()" [progress]="loadProgress"></loading-indicator>
     </div>
   `,
   styles: [''],
-  directives: [ROUTER_DIRECTIVES],
+  animations: {
+    ngLeave: [
+      style({opacity: 1, transform:'perspective(100px) translateZ(0)'}),
+      animate({opacity: 1, transform: 'perspective(100px) translateZ(-20px)'}, '0.05s 0 ease-in-out'),
+      animate({opacity: 0, transform: 'perspective(100px) translateZ(101px)'}, '0.3s 0 ease-in')
+    ]
+  },
+  directives: [ROUTER_DIRECTIVES, LoadingIndicator],
   providers: [
     Remote,
     Random,
@@ -33,11 +43,22 @@ import {Samples} from './samples.service';
   {path: '/ctrl', name: 'Control', component: Control}
 ])
 export class AppComponent {
+  loadProgress = 0;
+
   constructor(@Inject('size') private size) {
     this.onWindowResize()
+    let intr = setInterval(() => {
+      this.loadProgress += 100/6;
+      if (!this.isLoading()) {
+        clearInterval(intr);
+      }
+    }, 600);
   }
   onWindowResize() {
     this.size.width = window.innerWidth;
     this.size.height = window.innerHeight;
+  }
+  isLoading() {
+    return Math.round(this.loadProgress) < 100;
   }
 }
